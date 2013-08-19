f4e6789a083833a108ccf0215555e1ff5e4f0d4a
asdfs <sdfas>
2013-08-19 04:22:12 +0000

    bleh

--- CLI-script.php
+++ CLI-script.php
@@ -1,6 +1,8 @@
 <?php
-	$bleh = file_get_contents("https://api.twitch.tv/kraken/search/streams?q=starcraft");
-	$blah = json_decode($bleh);
-
-	print_r($blah);
+	$bleh = file_get_contents("https://api.twitch.tv/kraken/search/streams?q=Starcraft%20II&limit=10");
+	$blah = json_decode($bleh, true);
+	
+	foreach($blah["streams"] as $stream){
+		echo $stream["channel"]["status"]." - ".$stream["viewers"]."\n";
+	}
 ?>
